7747909455bd2c48c81692a03b509a93791994fc
zhuqi <zhuq96@hotmail.com>
2017-10-10 21:50:02 +0800

    modify summary

--- data_utils.py
+++ data_utils.py
@@ -4,31 +4,32 @@ import os
 import tensorflow as tf
 import numpy as np
 from tensorflow.python.framework import constant_op
+import time
 from seq2seq import seq2seq, _START_VOCAB, _PAD, _EOS
 
 FLAGS = tf.app.flags.FLAGS
 
-tf.app.flags.DEFINE_string("data_from", "MSCOCO.p1.test", "data_from")
-tf.app.flags.DEFINE_string("data_to", "MSCOCO.p2.test", "data_to")
+tf.app.flags.DEFINE_string("data_from", "MSCOCO.p1.train", "data_from")
+tf.app.flags.DEFINE_string("data_to", "MSCOCO.p2.train", "data_to")
 tf.app.flags.DEFINE_string("data_dir", "./data", "data_dir")
 tf.app.flags.DEFINE_string("train_dir", "./train", "train_dir")
 tf.app.flags.DEFINE_string("log_dir", "./log", "log_dir")
 tf.app.flags.DEFINE_string("attn_mode", "Luong", "attn_mode")
 
 tf.app.flags.DEFINE_boolean("use_lstm", True, "use_lstm")
-tf.app.flags.DEFINE_boolean("share_emb", True, "share_emb")
+tf.app.flags.DEFINE_boolean("share_emb", False, "share_emb")
 tf.app.flags.DEFINE_boolean("splited", True, "splited")
 tf.app.flags.DEFINE_boolean("is_train", True, "is_train")
 
 tf.app.flags.DEFINE_integer("batch_size", 100, "batch_size")
 tf.app.flags.DEFINE_integer("embed_size", 128, "embed_size")
-tf.app.flags.DEFINE_integer("num_units", 50, "num_units")
+tf.app.flags.DEFINE_integer("num_units", 64, "num_units")
 tf.app.flags.DEFINE_integer("num_layers", 1, "num_layers")
 tf.app.flags.DEFINE_integer("beam_width", 5, "beam_width")
-tf.app.flags.DEFINE_integer("vocab_size", 4000, "vocab_size")
-tf.app.flags.DEFINE_integer("train_size", 10000, "train_size")
-tf.app.flags.DEFINE_integer("dev_size", 5000, "dev_size")
-tf.app.flags.DEFINE_integer("test_size", 5000, "test_size")
+tf.app.flags.DEFINE_integer("vocab_size", 40000, "vocab_size")
+tf.app.flags.DEFINE_integer("train_size", 206902, "train_size")
+tf.app.flags.DEFINE_integer("valid_size", 10000, "valid_size")
+tf.app.flags.DEFINE_integer("test_size", 10000, "test_size")
 
 tf.app.flags.DEFINE_float("learning_rate", 0.001, "learning rate")
 tf.app.flags.DEFINE_float("keep_prob", 0.8, "keep_prob")
@@ -39,34 +40,34 @@ class data_process(object):
         self.data_dir = tfFLAGS.data_dir
         self.train_from = os.path.join(self.data_dir, 'train_from')
         self.train_to = os.path.join(self.data_dir, 'train_to')
-        self.dev_from = os.path.join(self.data_dir, 'dev_from')
-        self.dev_to = os.path.join(self.data_dir, 'dev_to')
+        self.valid_from = os.path.join(self.data_dir, 'valid_from')
+        self.valid_to = os.path.join(self.data_dir, 'valid_to')
         self.test_from = os.path.join(self.data_dir, 'test_from')
         self.test_to = os.path.join(self.data_dir, 'test_to')
         if not tfFLAGS.splited:
             self.data_from = os.path.join(self.data_dir,tfFLAGS.data_from)
             self.data_to = os.path.join(self.data_dir,tfFLAGS.data_to)
-            self._split(tfFLAGS.train_size, tfFLAGS.dev_size, tfFLAGS.test_size)
+            self._split(tfFLAGS.train_size, tfFLAGS.valid_size, tfFLAGS.test_size)
         self.vocab_size = tfFLAGS.vocab_size
 
-    def _split(self, train_size, dev_size, test_size):
-        total_size = train_size+dev_size+test_size
+    def _split(self, train_size, valid_size, test_size):
+        total_size = train_size+valid_size+test_size
         sel = random.sample(range(total_size), total_size)
-        dev_dict = {}.fromkeys(sel[:dev_size])
+        valid_dict = {}.fromkeys(sel[:valid_size])
         test_dict = {}.fromkeys(sel[-test_size:])
         train_from = open(self.train_from,'wb')
         train_to = open(self.train_to,'wb')
-        dev_from = open(self.dev_from,'wb')
-        dev_to = open(self.dev_to,'wb')
+        valid_from = open(self.valid_from,'wb')
+        valid_to = open(self.valid_to,'wb')
         test_from = open(self.test_from,'wb')
         test_to = open(self.test_to,'wb')
 
         with open(self.data_from) as ff, open(self.data_to) as ft:
             cntline = 0
             for post, resp in izip(ff,ft):
-                if cntline in dev_dict:
-                    dev_from.write(post)
-                    dev_to.write(resp)
+                if cntline in valid_dict:
+                    valid_from.write(post)
+                    valid_to.write(resp)
                 elif cntline in test_dict:
                     test_from.write(post)
                     test_to.write(resp)
@@ -77,8 +78,8 @@ class data_process(object):
 
         train_from.close()
         train_to.close()
-        dev_from.close()
-        dev_to.close()
+        valid_from.close()
+        valid_to.close()
         test_from.close()
         test_to.close()
         print "split completed"
@@ -103,8 +104,8 @@ class data_process(object):
     def load_train_data(self):
         return self.load_data(self.train_from,self.train_to)
 
-    def load_dev_data(self):
-        return self.load_data(self.dev_from, self.dev_to)
+    def load_valid_data(self):
+        return self.load_data(self.valid_from, self.valid_to)
 
     def load_test_data(self):
         return self.load_data(self.test_from, self.test_to)
@@ -145,12 +146,14 @@ class data_process(object):
                         'response_len': responses_length}
         return batched_data
 
-    def batchify(self,
-                 data,
-                 batch_size):
+    def train_batch(self, data, batch_size):
+        selected_data = [random.choice(data) for _ in range(batch_size)]
+        batched_data = self.gen_batched_data(selected_data)
+        return batched_data
+
+    def eval_batches(self, data, batch_size):
         for i in range(len(data)//batch_size):
-            selected_data = [random.choice(data) for _ in range(batch_size)]
-            batched_data = self.gen_batched_data(selected_data)
+            batched_data = self.gen_batched_data(data[i*batch_size:(i+1)*batch_size])
             yield batched_data
 
     def infer(self,
@@ -173,7 +176,7 @@ def main(unused_argv):
     # config.gpu_options.allow_growth = True
     dp = data_process(FLAGS)
     train_data = dp.load_train_data()
-    valid_data = dp.load_dev_data()
+    valid_data = dp.load_valid_data()
     test_data = dp.load_test_data()
 
     s2s = seq2seq(FLAGS)
@@ -188,36 +191,44 @@ def main(unused_argv):
                 s2s.initialize(sess, vocab=vocab)
 
             train_writer = tf.summary.FileWriter(os.path.join(FLAGS.train_dir, 'train'), sess.graph)
-            test_writer = tf.summary.FileWriter(os.path.join(FLAGS.train_dir, 'test'))
+            valid_writer = tf.summary.FileWriter(os.path.join(FLAGS.train_dir, 'valid'))
 
+            train_loss = 0
+            time_step = 0
             while True:
-                # Keep track of average train cost for this epoch
-                train_cost = 0
-                for batch in dp.batchify(train_data, FLAGS.batch_size):
-                    # print batch
-                    train_op, train_loss, train_ppl, global_step, summary = s2s.step(sess, batch, is_train=True)
-                    train_cost += train_loss
-                    train_writer.add_summary(summary=summary, global_step=global_step)
-                # print global_step
-                train_cost /= FLAGS.train_size // FLAGS.batch_size
+                global_step = s2s.global_step.eval()
+                if global_step % 1000 == 0:
+                    time_step /= 1000
+                    train_loss /= 1000
+                    print("global step %d step-time %.4f train loss %f perplexity %f"
+                          % (global_step, time_step, train_loss, np.exp(train_loss)))
+                    train_loss = 0
+                    time_step = 0
 
-                valid_batch = dp.gen_batched_data(valid_data)
-                # Test time
-                t = s2s.step(sess, valid_batch, is_train=False)
-                print 'post: ', ' '.join(t[2][0])
-                print 'response: ', ' '.join(t[3][0])
-                print 'train_out: ', ' '.join(t[4][0])
-                print 'infer_out: ', ' '.join(t[5][0])
-                print 'beam_out: ', ' '.join(t[6][0, :, 0])
+                    s2s.saver.save(sess, "%s/model.ckpt" % FLAGS.train_dir, global_step=global_step)
 
-                test_writer.add_summary(summary=t[8], global_step=t[7])
-                valid_cost = t[0]
-                # test_cost /= FLAGS.test_size / FLAGS.batch_size
-                print 'test ppl: ', np.exp(valid_cost)
+                    valid_loss = 0
+                    for batch in dp.eval_batches(valid_data,FLAGS.batch_size):
+                        [loss] = s2s.step(sess, batch, is_train=False)
+                        valid_loss += loss
+                    valid_loss /= FLAGS.valid_size // FLAGS.batch_size
+                    summary = tf.Summary()
+                    summary.value.add(tag='valid loss', simple_value=valid_loss)
+                    summary.value.add(tag='valid ppl', simple_value=np.exp(valid_loss))
+                    valid_writer.add_summary(summary, global_step)
+                    print "valid loss:", valid_loss, "valid ppl:", np.exp(valid_loss)
 
-                print("train loss:", train_cost, "test loss:", valid_cost)
-                print("train ppl:", np.exp(train_cost), "test ppl:", np.exp(valid_cost))
-                s2s.saver.save(sess, "%s/model.ckpt" % FLAGS.train_dir, global_step=global_step)
+                start_time = time.time()
+
+                train_batch = dp.train_batch(train_data, FLAGS.batch_size)
+                train_op, loss, global_step = s2s.step(sess, train_batch, is_train=True)
+                train_loss += loss
+                summary = tf.Summary()
+                summary.value.add(tag='train loss', simple_value=loss)
+                summary.value.add(tag='train ppl', simple_value=np.exp(loss))
+                train_writer.add_summary(summary=summary, global_step=global_step)
+
+                time_step += (time.time() - start_time)
 
         else:
             s2s.saver.restore(sess, tf.train.latest_checkpoint(FLAGS.train_dir))
